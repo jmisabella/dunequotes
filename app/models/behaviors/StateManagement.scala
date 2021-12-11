529ee0a6130482f95c6246b1e45f12d2d98da85d
Jeffrey <jeff.isabella@gmail.com>
2021-12-11 17:43:24 -0500

    Refactored State and StateManagement
    Refactored State and StateManagement

--- app/models/behaviors/StateManagement.scala
+++ app/models/behaviors/StateManagement.scala
@@ -19,16 +19,34 @@ trait StateManagement {
   val rollover: _TimeRollover
   val getQuote: _GetQuote 
 
+  def initialState(quotesConfigPath: String, historyConfigPath: String, historyLimit: Int = 60, rolloverTimeIntervalUnit: Int = Calendar.DATE, randomSeed: Int = Random.nextInt()): Either[String, State] = {
+    val quotes = reader.readFile(quotesConfigPath)
+    val history = reader.readFile(historyConfigPath)
+    (quotes, history) match {
+      case (Left(q), Left(h)) => Left(s"Error reading both quotes and history: [$q], [$h]")
+      case (Left(q), _) => Left(s"Error reading quotes: [$q]")
+      case (_, Left(h)) => Left(s"Error reading history: [$h]")
+      case (Right(rawQuotes), Right(rawHistory)) => (serialization.parse(rawQuotes), serialization.parse(rawHistory)) match {
+        case (Left(q), Left(h)) => Left(s"Error deserializing both quotes and history: [$q], [$h]")
+        case (Left(q), _) => Left(s"Error deserializing quotes: [$q]") 
+        case (_, Left(h)) => Left(s"Error deserializing history: [$h]")
+        case (Right(qs), Right(hs)) => reader.lastModified(historyConfigPath) match { // determine time of last rollover 
+          case Left(m) => Left(m)
+          case Right(m) => nextState(State(qs, hs, historyLimit, RNG.RandomSeed(randomSeed), historyConfigPath, m, rolloverTimeIntervalUnit))
+        }
+      }
+    }
+  }
+
   // rotate history if neccessary, will either return existing state or the next state if history is rotated
-  def state(currentState: State, quotesConfigPath: String, historyConfigPath: String, historyLimit: Int = 60, rolloverTimeIntervalUnit: Int = Calendar.DATE, randomSeed: Int = Random.nextInt()): Either[String, State] = {
+  def nextState(currentState: State): Either[String, State] = {
     currentState match {
-      case null => initialState(quotesConfigPath, historyConfigPath, historyLimit, randomSeed)
+      case null => Left(s"State is missing")
       case s => {
         // now need to see if it is time to rollover
         val format = "yyyy-MM-dd HH:mm:ss.SSS"
         val currentTime = new SimpleDateFormat(format).format(new Date())
-        println("CALENDAR UNIT: " + rolloverTimeIntervalUnit)
-        val isRollover = rollover.isRollover(currentState.rolloverTime, currentTime, format, rolloverTimeIntervalUnit)
+        val isRollover = rollover.isRollover(currentState.rolloverTime, currentTime, format, currentState.rolloverTimeIntervalUnit)
         // val isRollover = rollover.isRollover(currentState.rolloverTime, currentTime, format, Calendar.DATE)
         isRollover match {
           case false => Right(s) // not yet time to rollover
@@ -36,9 +54,9 @@ trait StateManagement {
             // time to rollover, so determine next featured quote and write it to history config 
             val nextState: State = getQuote.rotateFeaturedQuote(s) 
             try {
-              writer.writeToFile(historyConfigPath, serialization.json(nextState.history))
+              writer.writeToFile(nextState.historyConfigPath, serialization.json(nextState.history))
             } catch {
-              case e: Exception => Left(s"Error writing history to [$historyConfigPath]: ${e.getMessage()}")
+              case e: Exception => Left(s"Error writing history to [$nextState.historyConfigPath]: ${e.getMessage()}")
             }
             Right(nextState.copy(rolloverTime = currentTime))  
           }
@@ -46,7 +64,7 @@ trait StateManagement {
       }
     } 
   }
-
+  
   // def state(currentState: State, quotesConfigPath: String, historyConfigPath: String, historyLimit: Int = 60, rolloverTimeIntervalUnit: Int = Calendar.DATE, randomSeed: Int = Random.nextInt()): Either[String, State] = {
   //   currentState match {
   //     case null => initialState(quotesConfigPath, historyConfigPath, historyLimit, randomSeed)
@@ -74,29 +92,4 @@ trait StateManagement {
   //   } 
   // }
 
-  def initialState(quotesConfigPath: String, historyConfigPath: String, historyLimit: Int = 60, rolloverTimeIntervalUnit: Int = Calendar.DATE, randomSeed: Int = Random.nextInt()): Either[String, State] = {
-    val quotes = reader.readFile(quotesConfigPath)
-    val history = reader.readFile(historyConfigPath)
-    (quotes, history) match {
-      case (Left(q), Left(h)) => Left(s"Error reading both quotes and history: [$q], [$h]")
-      case (Left(q), _) => Left(s"Error reading quotes: [$q]")
-      case (_, Left(h)) => Left(s"Error reading history: [$h]")
-      case (Right(rawQuotes), Right(rawHistory)) => (serialization.parse(rawQuotes), serialization.parse(rawHistory)) match {
-        case (Left(q), Left(h)) => Left(s"Error deserializing both quotes and history: [$q], [$h]")
-        case (Left(q), _) => Left(s"Error deserializing quotes: [$q]") 
-        case (_, Left(h)) => Left(s"Error deserializing history: [$h]")
-        case (Right(qs), Right(hs)) => reader.lastModified(historyConfigPath) match { // determine time of last rollover 
-          case Left(m) => Left(m)
-          case Right(m) => {
-            state(
-              State(qs, hs, historyLimit, RNG.RandomSeed(randomSeed), m, rolloverTimeIntervalUnit), 
-              quotesConfigPath, 
-              historyConfigPath, 
-              historyLimit, 
-              rolloverTimeIntervalUnit)
-          }
-        }
-      }
-    }
-  }
 }
